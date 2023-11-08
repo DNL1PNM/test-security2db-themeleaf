046b348ef0cf93d3dabd269d7c0fea76449e7d20
Daniil <babosinko@gmail.com>
2023-11-09 02:25:46 +0500

    Initial commit

---
+++ src/main/java/com/example/testsecurity2dbthemeleaf/repository/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.example.testsecurity2dbthemeleaf.repository;
+import org. springframework.data.jpa.repository.JpaRepository;
+import com.example.testsecurity2dbthemeleaf.entity.Role;
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/example/testsecurity2dbthemeleaf/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.testsecurity2dbthemeleaf.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.testsecurity2dbthemeleaf.entity.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);}
